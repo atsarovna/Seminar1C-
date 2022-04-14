64ef1cbcbad932651669362b3da242c6ad1c0795
Jane <evgenya.mun@yandex.ru>
2022-04-14 19:57:36 +0300

    add files

---
+++ квадрат числа/Program.cs
@@ -0,0 +1,8 @@
+﻿{
+    Console.Write("Введите число :");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Квадрат числа :");
+    Console.WriteLine(number * number);
+}
+//вывести квадрат числа
