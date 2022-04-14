643086c5b335ba753c7806efa2fdea2890eacacf
Jane <evgenya.mun@yandex.ru>
2022-04-14 20:47:17 +0300

    deleted

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿{
-    Console.Write("Введите число :");
-    int number = Convert.ToInt32(Console.ReadLine());
-
-    Console.Write("Квадрат числа :");
-    Console.WriteLine(number * number);
-}
-//вывести квадрат числа
