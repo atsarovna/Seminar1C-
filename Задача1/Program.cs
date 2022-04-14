4af45598143c275697c63ba975c73d0a0173661f
Jane <evgenya.mun@yandex.ru>
2022-04-14 18:13:30 +0300

    add solution of задача1

---
+++ Задача1/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+    Console.WriteLine("Введите число a :");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите число b :");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+
+    if(numberA * numberA == numberB)
+    {
+    Console.WriteLine("Да");
+    }
+    else
+    {
+    Console.Write("Нет");
+    }
+}
+//Напишите программу, которая на вход принимает два числа и проверяет, является ли второе число квадратом первого
